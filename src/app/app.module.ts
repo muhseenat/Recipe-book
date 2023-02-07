ce1c4e2369a6a249a2b94052f597578a0834447b
muhseenat <mucktd777@gmail.com>
2023-02-07 11:49:51 +0530

    Started create UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {Headercomponent} from './components/header/header.component'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
-import { MyNewFeatureComponent } from './my-new-feature/my-new-feature.component';
 import { RecipeListComponent } from './components/recipe/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './components/recipe/recipe-item/recipe-item.component';
 import { RecipeDetailsComponent } from './components/recipe/recipe-details/recipe-details.component';
-import { ShoppingComponent } from './components/shopping/shopping.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
+    Headercomponent,
     AppComponent,
     RecipeComponent,
-    MyNewFeatureComponent,
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailsComponent,
-    ShoppingComponent,
     ShoppingListComponent,
     ShoppingEditComponent
   ],
