9a76bafbff8ce949da152993559161d3f6e85cf4
muhseenat <mucktd777@gmail.com>
2023-02-07 10:55:10 +0530

    Created new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { MyNewFeatureComponent } from './my-new-feature/my-new-feature.component';
+import { RecipeListComponent } from './components/recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './components/recipe/recipe-details/recipe-details.component';
+import { ShoppingComponent } from './components/shopping/shopping.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    MyNewFeatureComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    ShoppingComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
