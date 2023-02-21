5bc6674de44004ef63b6f8bacb1141dbf9ba5a3d
muhseenat <mucktd777@gmail.com>
2023-02-21 15:13:50 +0530

    chore:Solved the issue of not taking the material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {Headercomponent} from './components/header/header.component'
+import { Headercomponent } from './components/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
@@ -9,10 +9,10 @@ import { RecipeItemComponent } from './components/recipe/recipe-item/recipe-item
 import { RecipeDetailsComponent } from './components/recipe/recipe-details/recipe-details.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     Headercomponent,
@@ -28,11 +28,12 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     MatSlideToggleModule,
     BrowserModule,
     AppRoutingModule,
-    MatIconModule,
-    MatToolbarModule,
-
+    MatToolbarModule ,
+    MatIconModule, 
+    MatButtonModule
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
